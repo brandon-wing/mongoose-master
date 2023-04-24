31d695c0fa25fad6a957521155ae856758025aaa
rudyxwhite <brandonrwing98@gmail.com>
2023-04-24 11:18:12 -0500

    usersroutes completed

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
     //find all users will be used in the GET function in the usersRoutes for the '/' endpoint
@@ -18,6 +18,46 @@ module.exports = {
         User.create(req.body)
         .then((newUser) => res.json(newUser))
         .catch((err) => res.status(500).json(err))
-      }
+      },
+      //updating a user's info, this will be the PUT request
+      updateUserInfo(req, res) {
+        User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body }
+        )
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err))
 
+      },
+      //delete a user by its ID
+      deleteOneUser(req, res) {
+      //once the user is deleted, all of the thoughts with its associated userId will be too!
+        User.findOneAndDelete({ _id: req.params.userId })
+          .then((deletedUser) =>
+              Thought.deleteMany({ _id: { $in: deletedUser.thoughts } })
+              )
+              .then(() => res.json({ message: 'Success! User is no longer in the database.' }))
+              .catch((err) => res.status(500).json(err))
+
+      },
+      //add a friend POST request
+      addFriend(req, res){
+        User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $addToSet: { friends: req.body } }
+        )
+          .then(() => res.json({message: 'Added to friends list!'}))
+          .catch((err) => res.status(500).json(err))
+        
+      },
+      deleteFriend(req, res){
+      User.findOneAndUpdate(
+        { _id: req.params.userId },
+        //removes from the friends field the user by its specific id
+        { $pull: { friends: req.params.friendId } }
+        )
+        .then(() => res.json({message: 'deleted from friends list!'}))
+        .catch((err) => res.status(500).json(err))
+
+    }
 }
--- routes/apiRoutes/usersRoutes.js
+++ routes/apiRoutes/usersRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const {findAllUsers, findOneUser, createNewUser} = require('../../controllers/usersController');
+const {findAllUsers, findOneUser, createNewUser, deleteOneUser, addFriend, deleteFriend} = require('../../controllers/usersController');
 //here are the GET and POST requests within '/' endpoint, found in the usersController
 router.route('/').get(findAllUsers).post(createNewUser);
-//here is a GET request for finding a specific user by ID at the '/:userId' endpoint
-router.route('/:userId').get(findOneUser);
-
+//here is a GET + DELETE request for finding/deleting a specific user by ID at the '/:userId' endpoint
+router.route('/:userId').get(findOneUser).delete(deleteOneUser);
+//here are POST and DELETE requests for adding and deleting a friend
+router.route('/:userId/friends').post(addFriend)
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
 module.exports = router;
